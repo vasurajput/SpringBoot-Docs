4e48756837b4cc60d25ebf9c243ff1458646f397
vasurajput <vashurajput005@gmail.com>
2026-07-12 20:27:33 +0530

    Revert "Merge branch 'master' of https://github.com/vasurajput/SpringBoot-Docs"
    This reverts commit 30931dddff6b3e17d5a884cfebc405f51ec69a0e, reversing
    changes made to 09cd9910d45fdd6a663f57586145da6ef57afdb3.

--- src/main/java/com/pushpay/gen/excel/SheetData.java
+++
@@ -1,7 +0,0 @@
-package com.pushpay.gen.excel;
-
-import java.util.List;
-
-/** Raw contents of one Excel sheet: the header names found (order as in the file) and data rows. */
-public record SheetData(String sheetName, List<String> headers, List<ExcelRow> rows) {
-}
